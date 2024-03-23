5c60911babc6c433583c03fa9dadfc607ce17d47
MiguelArbelaez0 <119169682+MiguelArbelaez0@users.noreply.github.com>
2024-03-22 21:42:16 -0500

    --

--- book_app/test/domain/use_case/add_favorite_book_use_case_test.dart
+++ book_app/test/domain/use_case/add_favorite_book_use_case_test.dart
@@ -1,11 +1,14 @@
 import 'package:book_app/domain/use_cases/add_favorite_book_use.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail/mocktail.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 import '../../fake_data.dart';
 import '../../mocks.dart';
 
 void main() {
+  // TestWidgetsFlutterBinding.ensureInitialized();
+  // SharedPreferences.setMockInitialValues({});
   late MockAddFavoriteBookRepository _mockAddFavoriteBookRepository;
   late AddFavoriteBookUseCase _addFavoriteBookUseCase;
 
@@ -18,6 +21,7 @@ void main() {
         .thenAnswer((_) => Future.value());
 
     await _mockAddFavoriteBookRepository.addBookToFavorites(documentFake);
+    // await _addFavoriteBookUseCase.invokeAddFavoriteBook(documentFake);
 
     verify(
         () => _mockAddFavoriteBookRepository.addBookToFavorites(documentFake));
